39060ef1cd0ff7ff45a559778651c482eaaa5f02
Davuthan İşçi <hoidxdev@gmail.com>
2024-04-15 11:03:04 +0300

    Removed Orbitcontrols.

--- src/pages/home/experience/helpers.tsx
+++ src/pages/home/experience/helpers.tsx
@@ -1,10 +1,9 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 
 export default function Helpers() {
     return (
         <>
             <Environment preset='sunset' />
-            <OrbitControls />
         </>
     )
 }
